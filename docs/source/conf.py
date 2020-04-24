d24ca49d0f2dfbc568c90b1cadfccece69270dde
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-04-23 20:19:25 -0500

    Fixing error in configuration file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,6 @@ author = u'Jeremiah C Leary'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.coverage', 'sphinx.ext.autodoc']
-]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
